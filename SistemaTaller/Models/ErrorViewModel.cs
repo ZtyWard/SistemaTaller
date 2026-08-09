1362d6f37acc168bc543a765a11b6d47deecb915
BotBeyond <150319467+ZtyWard@users.noreply.github.com>
2026-08-09 14:40:34 -0600

    Agregar archivos de proyecto.

---
+++ SistemaTaller/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaTaller.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
